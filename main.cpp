91e7b895c4abc09c0dedf93fe2981ea478c8ee2b
MDC <120914873+MDC15@users.noreply.github.com>
2023-11-04 20:13:06 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,108 +1,104 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
+#define WHITE 'O'
+#define BLACK 'X'
 
-// Khai báo cấu trúc ô bàn cờ
-struct Cell {
-  char value;
-  bool isOccupied;
-};
-
-// Khai báo lớp bàn cờ
-class Board {
-public:
-  // Hàm khởi tạo
-  Board(int n) {
-    board_ = vector<vector<Cell>>(n, vector<Cell>(n));
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        board_[i][j].value = ' ';
-        board_[i][j].isOccupied = false;
-      }
-    }
-  }
-
-  // Hàm đọc bàn cờ từ tệp
-  void ReadBoard(char *path) {
-    fstream f(path);
-    int n;
-    f >> n;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        f >> board_[i][j].value;
-        if (board_[i][j].value == 'O' || board_[i][j].value == 'X') {
-          board_[i][j].isOccupied = true;
+int N;
+char **board;
+int **visited;  
+void readBoard(char *path) {
+    FILE *file = fopen(path, "r");
+    
+    fscanf(file, "%d\n", &N);
+    board = (char **)malloc(N * sizeof(char *));
+    visited = (int **)malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++) {
+        board[i] = (char *)malloc((N + 1) * sizeof(char));
+        visited[i] = (int *)calloc(N, sizeof(int));  
+        for (int j = 0; j < N; j++) {
+            fscanf(file, " %c", &board[i][j]);
         }
-      }
+        board[i][N] = '\0';  
     }
-  }
 
-  // Hàm kiểm tra xem ô (i, j) có bị bao vây hay không
-  bool IsCellSurrounded(int i, int j) {
-    // Kiểm tra các ô xung quanh
-    for (int x = i - 1; x <= i + 1; x++) {
-      for (int y = j - 1; y <= j + 1; y++) {
-        if (x >= 0 && x < board_.size() && y >= 0 && y < board_[0].size()) {
-          if (board_[x][y].value == board_[i][j].value) {
-            return false;
-          }
+    fclose(file);
+}
+
+void freeBoard() {
+    for (int i = 0; i < N; i++) {
+        free(board[i]);
+        free(visited[i]);
+    }
+    free(board);
+    free(visited);
+}
+
+int isSurrounded(int x, int y) {
+    if (x < 0 || x >= N || y < 0 || y >= N) {
+        return 0;  
+    }
+    if (board[x][y] == BLACK || visited[x][y]) {
+        return 1;  
+    }
+    if (board[x][y] == WHITE) {
+        visited[x][y] = 1;  
+        int up = isSurrounded(x, y - 1);
+        int down = isSurrounded(x, y + 1);
+        int left = isSurrounded(x - 1, y);
+        int right = isSurrounded(x + 1, y);
+        return up && down && left && right;
+    }
+    return 0;
+}
+
+void capture(int x, int y) {
+    if (x < 0 || x >= N || y < 0 || y >= N || board[x][y] != WHITE || !visited[x][y]) {
+        return;
+    }
+    board[x][y] = BLACK;
+    capture(x, y - 1);
+    capture(x, y + 1);
+    capture(x - 1, y);
+    capture(x + 1, y);
+}
+
+int getWinner(char *path) {
+    readBoard(path);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j] == WHITE && !visited[i][j] && isSurrounded(i, j)) {
+                capture(i, j);
+            }
         }
-      }
     }
-    return true;
-  }
 
-  // Hàm tính diện tích vùng đất của quân trắng và đen
-  int CountArea(char value) {
-    int area = 0;
-    for (int i = 0; i < board_.size(); i++) {
-      for (int j = 0; j < board_[0].size(); j++) {
-        if (board_[i][j].value == value && board_[i][j].isOccupied) {
-          area++;
-          // Di chuyển quân trắng bị bao vây sang quân đen
-          if (value == 'O' && IsCellSurrounded(i, j)) {
-            board_[i][j].value = 'X';
-          }
+    int whiteCount = 0;
+    int blackCount = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (board[i][j] == WHITE) {
+                whiteCount++;
+            } else if (board[i][j] == BLACK) {
+                blackCount++;
+            }
         }
-      }
     }
-    return area;
-  }
 
-  // Hàm trả về bên thắng cuộc
-  int GetWinner() {
-    int area_white = CountArea('O');
-    int area_black = CountArea('X');
-    if (area_white > area_black) {
-      return 1;
-    } else if (area_white < area_black) {
-      return -1;
+    freeBoard();
+
+    if (whiteCount > blackCount) {
+        return 1;
+    } else if (whiteCount < blackCount) {
+        return -1;
     } else {
-      return 0;
+        return 0;
     }
-  }
-
-private:
-  // Mảng lưu trữ bàn cờ
-  vector<vector<Cell>> board_;
-};
+}
 
 int main() {
-  // Đọc bàn cờ từ tệp
-  Board board(5);
-  board.ReadBoard("input.txt");
-
-  // Xác định bên thắng cuộc
-  int winner = board.GetWinner();
-
-  // In kết quả
-  if (winner == 1) {
-    cout << "Quân trắng thắng." << endl;
-  } else if (winner == -1) {
-    cout << "Quân đen thắng." << endl;
-  } else {
-    cout << "Hai bên hòa." << endl;
-  }
-  return 0;
+    printf("%d\n", getWinner("input.txt"));
+    return 0;
 }
