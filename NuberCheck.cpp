a4d295519b0207a63fa465c9ed2f3d22edf0f1ae
MDC <120914873+MDC15@users.noreply.github.com>
2023-11-07 20:53:47 +0700

    Update

--- NuberCheck.cpp
+++ NuberCheck.cpp
@@ -2,21 +2,9 @@
 #include <math.h>
 using namespace std;
 
-bool isPrime(int p) {
-    if(p < 2) {
-        return false;
-    }
-    for(int i = 2; i <= sqrt(p); i++) {
-        if(p % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 bool isMersenne(int p) {
     int m = log2(p + 1);
-    return isPrime(m) && (p == pow(2, m) - 1);
+    return (m) && (p == pow(2, m) - 1);
 }
 
 int main() {
