d10e16b3a1b21b1634abff2c361b3f850d8a06e3
MDC <120914873+MDC15@users.noreply.github.com>
2023-11-23 13:09:45 +0700

    Update WorkWithFiles.cpp

--- WorkWithFiles.cpp
+++ WorkWithFiles.cpp
@@ -1,86 +1,45 @@
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
-// Định nghĩa cấu trúc cho sinh viên
-struct SinhVien
-{
-    int stt;
-    std::string ho;
-    std::string ten;
-    std::string ngaySinh;
-    int daiSo;
-    int thcs4;
-    int thcs1;
-};
-
-// Hàm kiểm tra điểm THCS4 bằng 10 và in ra họ tên của sinh viên
-void prafel10(const std::string &inputPath, const std::string &outputPath)
-{
-    std::ifstream inputFile(inputPath);
-    if (!inputFile.is_open())
-    {
-        std::cout << "Khong mo duoc file dau vao." << std::endl;
+void grade10(const char* path) {
+    std::ifstream file(path);
+    if (!file) {
+        std::cout << "Không thể mở tệp tin." << std::endl;
         return;
     }
 
-    std::ofstream outputFile(outputPath);
-    if (!outputFile.is_open())
-    {
-        std::cout << "Khong tao duoc file dau ra." << std::endl;
-        inputFile.close();
-        return;
-    }
+    std::vector<std::string> students;
 
-    int count = 0;
     std::string line;
-
-    // Đọc từng dòng trong file đầu vào
-    while (std::getline(inputFile, line))
-    {
-        SinhVien sv;
-
-        // Sử dụng stringstream để đọc từng trường thông tin từ dòng
+    while (std::getline(file, line)) {
         std::istringstream iss(line);
-        char comma;
-
-        iss >> sv.stt >> comma;
-        std::getline(iss, sv.ho, ',');
-        std::getline(iss, sv.ten, ',');
-        std::getline(iss, sv.ngaySinh, ',');
-        iss >> sv.daiSo >> comma >> sv.thcs4 >> comma >> sv.thcs1;
-
-        // Kiểm tra điểm THCS4 bằng 10
-        if (sv.thcs4 == 10)
-        {
-            // In ra họ tên của sinh viên
-            std::cout << sv.ho << " " << sv.ten << std::endl;
-
-            // Ghi thông tin vào file đầu ra
-            outputFile << sv.ho << " " << sv.ten << std::endl;
-            count++;
+        std::string name, grade;
+        std::getline(iss, name, ',');
+        std::getline(iss, grade, ',');
+        if (grade == "10") {
+            students.push_back(name);
         }
     }
 
-    inputFile.close();
-    outputFile.close();
+    file.close();
 
-    // Kiểm tra nếu không có sinh viên nào có điểm THCS4 bằng 10
-    if (count == 0)
-    {
-        std::cout << "Khong co sinh vien nao co diem THCS4 bang 10." << std::endl;
+    if (students.empty()) {
+        std::cout << "Không có sinh viên nào có điểm THCS4 là 10." << std::endl;
+        return;
     }
-    else
-    {
-        std::cout << "\nDa luu du lieu vao file 1.csv.";
+
+    std::cout << "Các sinh viên có điểm THCS4 là 10:" << std::endl;
+    for (const std::string& student : students) {
+        std::cout << student << std::endl;
     }
 }
 
-int main()
-{
-    std::string inputPath = "./BANGDIEM.txt";
-    std::string outputPath = "./1.csv";
-    prafel10(inputPath, outputPath);
-
+int main() {
+    const char* path = "1.csv";
+    grade10(path);
     return 0;
 }
+
